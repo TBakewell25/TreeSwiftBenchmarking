154b290be7a51fe99bf637d7b964c764096ba82c
Thomas Bakewell <cbakewell1@gmail.com>
2025-11-17 23:49:16 -0500

    lets try this again

---
+++ TreeSwift-Paper/scripts/figures.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+MEMORY_DEN = 1024*1024
+
+# load data
+from pickle import load
+from sys import argv
+if len(argv) != 2:
+    print("USAGE: %s <data.pkl>"%argv[0]); exit(1)
+if argv[1].lower().endswith('.gz'):
+    from gzip import open as gopen
+    data = load(gopen(argv[1]))
+else:
+    data = load(open(argv[1]))
+
+# set up seaborn
+from matplotlib import rcParams
+from matplotlib.lines import Line2D
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+sns.set_context("paper", rc={"font.size":12,"axes.titlesize":16,"axes.labelsize":14,"legend.fontsize":10,"xtick.labelsize":10,"ytick.labelsize":10})
+sns.set_style("ticks")
+rcParams['font.family'] = 'serif'
+T = ['DendroPy','Bio.Phylo','ETE Toolkit','TreeSwift', 'CompactTree']
+name_to_key = {'DendroPy':'dendropy', 'Bio.Phylo':'biophylo', 'TreeSwift':'treeswift', 'ETE Toolkit':'ete3', 'CompactTree':'compacttree'}
+pal = {'TreeSwift':'black', 'Bio.Phylo':'red', 'DendroPy':'blue', 'ETE Toolkit':'green', 'CompactTree':'purple'}
+linestyle = {'TreeSwift':'-', 'DendroPy':':', 'Bio.Phylo':'--', 'ETE Toolkit':'-.', 'CompactTree':'-.'}
+meta = {
+    'name': {
+        'distance_matrix': "Distance Matrix",
+        'inorder': "In-Order Traversal",
+        'ladderize': "Ladderize",
+        'levelorder': "Level-Order Traversal",
+        'load_tree': "Load Tree",
+        'memory': "Memory Usage",
+        'mrca': "Most Recent Common Ancestor",
+        'postorder': "Post-Order Traversal",
+        'preorder': "Pre-Order Traversal",
+        'rootdistorder': "Root-Distance-Order Traversal",
+        'total_branch_length': "Total Length"
+    }
+}
+N = None
+for m in data:
+    if N is None or len(data[m]) > len(N):
+        N = sorted(data[m].keys())
+
+# create figures
+for m in sorted(data.keys()):
+    fig = plt.figure()
+    used = []
+    for t in T:
+        x = []; y = []
+        for n in N:
+            if n not in data[m]:
+                continue
+            if name_to_key[t] in data[m][n]:
+                x += [n]*10
+                y += data[m][n][name_to_key[t]]
+            else:
+                continue
+        if len(x) != 0:
+            x = np.array(x); y = np.array(y)
+            if m == 'memory':
+                y = y/float(MEMORY_DEN)
+            sns.pointplot(x=x, y=y, color=pal[t], linestyles=linestyle[t])
+            used.append(t)
+    plt.yscale('log')
+    plt.title(meta['name'][m])
+    plt.xlabel("Number of Leaves")
+    if m == 'memory':
+        plt.ylabel("Memory Usage (MB)")
+    else:
+        plt.ylabel("Execution Time (seconds)")
+    handles = [Line2D([0],[0],color=pal[t],label=t,linestyle=linestyle[t],linewidth=1.5) for t in used]
+    legend = plt.legend(handles=handles,bbox_to_anchor=(0.005, 0.995), loc=2, borderaxespad=0., frameon=True)
+    plt.show()
+    fig.savefig('%s.pdf'%m.lower(), format='pdf', bbox_inches='tight')
---
+++ TreeSwift-Paper/scripts/time.py
@@ -0,0 +1,413 @@
+#!/usr/bin/env python3
+import collections; from collections.abc import MutableMapping; collections.MutableMapping = MutableMapping
+from Bio import Phylo
+from itertools import combinations
+from treeswift import read_tree_newick
+import dendropy
+import ete3
+import numpy
+from CompactTree import compact_tree, traverse_postorder, traverse_preorder
+NA = "NA" # what to print when function is not implemented
+
+# get memory usage
+def memory():
+    from os import getpid; from psutil import Process
+    return Process(getpid()).memory_inforeturn Process(getpid()).memory_info().rss().rss
+
+# distance matrix in ETE Toolkit
+# obtained from https://github.com/linsalrob/EdwardsLab/blob/master/trees/tree_to_cophenetic_matrix.py
+def distance_matrix_ete(tree):
+    leaves = tree.get_leaves()
+    paths = {x:set() for x in leaves}
+    for n in leaves:
+        if n.is_root():
+            continue
+        movingnode = n
+        while not movingnode.is_root():
+            paths[n].add(movingnode); movingnode = movingnode.up
+    leaf_distances = {x.name:{} for x in leaves}
+    for (leaf1, leaf2) in combinations(leaves, 2):
+        uniquenodes = paths[leaf1] ^ paths[leaf2]
+        distance = sum(x.dist for x in uniquenodes)
+        leaf_distances[leaf1.name][leaf2.name] = distance
+        leaf_distances[leaf2.name][leaf1.name] = distance
+    return leaf_distances
+
+# main code
+from io import StringIO
+from sys import argv
+from time import time
+if len(argv) != 4 or argv[2] not in {'treeswift','dendropy','biophylo','ete3', 'compacttree'}:
+    print("USAGE: %s <tree_file> <treeswift_or_dendropy_or_biophylo_or_ete3> <task>"%argv[0]); exit(1)
+if argv[1].lower().endswith('.gz'):
+    from gzip import open as gopen
+    treestr = gopen(argv[1]).read().decode().strip()
+else:
+    treestr = open(argv[1]).read().strip()
+treeio = StringIO(treestr) # for Bio.Phylo
+
+# ladderize
+def ladderize(m):
+    if m == 'dendropy':
+        tree = dendropy.Tree.get(data=treestr, schema='newick')
+        t_start = time()
+        tree.ladderize()
+        t_end = time()
+    elif m == 'biophylo':
+        tree = Phylo.read(treeio, 'newick')
+        t_start = time()
+        tree.ladderize()
+        t_end = time()
+    elif m == 'treeswift':
+        tree = read_tree_newick(treestr)
+        t_start = time()
+        tree.ladderize()
+        t_end = time()
+    elif m == 'ete3':
+        tree = ete3.Tree(treestr,format=1)
+        t_start = time()
+        tree.ladderize()
+        t_end = time()
+    elif m == 'compacttree':
+        return NA
+    else:
+        assert False, "Invalid tool: %s"%m
+    return t_end-t_start
+
+# distance matrix
+def distance_matrix(m):
+    if m == 'dendropy':
+        tree = dendropy.Tree.get(data=treestr, schema='newick')
+        t_start = time()
+        tree.phylogenetic_distance_matrix()
+        t_end = time()
+    elif m == 'biophylo':
+        # from http://biopython.org/wiki/Phylo_cookbook
+        tree = Phylo.read(treeio, 'newick')
+        t_start = time()
+        allclades = list(tree.find_clades(order='level'))
+        lookup = {}
+        for i, elem in enumerate(allclades):
+            lookup[elem] = i
+        distmat = numpy.repeat(numpy.inf, len(allclades)**2)
+        distmat.shape = (len(allclades), len(allclades))
+        for parent in tree.find_clades(terminal=False, order='level'):
+            for child in parent.clades:
+                if child.branch_length:
+                    distmat[lookup[parent], lookup[child]] = child.branch_length
+        if not tree.rooted:
+            distmat += distmat.transpose()
+        numpy.matrix(distmat)
+        t_end = time()
+    elif m == 'treeswift':
+        tree = read_tree_newick(treestr)
+        t_start = time()
+        tree.distance_matrix()
+        t_end = time()
+    elif m == 'ete3':
+        tree = ete3.Tree(treestr,format=1)
+        t_start = time()
+        distance_matrix_ete(tree)
+        t_end = time()
+    elif m == 'compacttree':
+        return NA
+    else:
+        assert False, "Invalid tool: %s"%m
+    return t_end-t_start
+
+# inorder traversal
+def inorder(m):
+    if m == 'dendropy':
+        tree = dendropy.Tree.get(data=treestr, schema='newick')
+        t_start = time()
+        for node in tree.inorder_node_iter():
+            pass
+        t_end = time()
+    elif m == 'biophylo':
+        return NA
+    elif m == 'treeswift':
+        tree = read_tree_newick(treestr)
+        t_start = time()
+        for node in tree.traverse_inorder():
+            pass
+        t_end = time()
+    elif m == 'ete3':
+        return NA
+    elif m == 'compacttree':
+        return NA
+        '''
+        tree = compact_tree(argv[1][:-3])
+        total = 0.
+        start = time()
+        for node in traverse_inorder(tree):
+            total += tree.get_edge_length(node)
+        end = time()
+        '''
+    else:
+        assert False, "Invalid tool: %s"%m
+    return t_end-t_start
+
+# level-order traversal
+def levelorder(m):
+    if m == 'dendropy':
+        tree = dendropy.Tree.get(data=treestr, schema='newick')
+        t_start = time()
+        for node in tree.levelorder_node_iter():
+            pass
+        t_end = time()
+    elif m == 'biophylo':
+        tree = Phylo.read(treeio, 'newick')
+        t_start = time()
+        for node in tree.find_clades(order='level'):
+            pass
+        t_end = time()
+    elif m == 'treeswift':
+        tree = read_tree_newick(treestr)
+        t_start = time()
+        for node in tree.traverse_levelorder():
+            pass
+        t_end = time()
+    elif m == 'ete3':
+        tree = ete3.Tree(treestr,format=1)
+        t_start = time()
+        for node in tree.traverse(strategy='levelorder'):
+            pass
+        t_end = time()
+    elif m == 'compacttree':
+        return NA        
+    else:
+        assert False, "Invalid tool: %s"%m
+    return t_end-t_start
+
+# MRCA
+def mrca(m):
+    if m == 'dendropy':
+        tree = dendropy.Tree.get(data=treestr, schema='newick')
+        t_start = time()
+        leaves = {l.taxon for l in tree.leaf_node_iter()}
+        tree.mrca(taxa=leaves)
+        t_end = time()
+    elif m == 'biophylo':
+        tree = Phylo.read(treeio, 'newick')
+        t_start = time()
+        leaves = tree.get_terminals()
+        tree.common_ancestor(leaves)
+        t_end = time()
+    elif m == 'treeswift':
+        tree = read_tree_newick(treestr)
+        t_start = time()
+        leaves = {str(l) for l in tree.traverse_leaves()}
+        tree.mrca(leaves)
+        t_end = time()
+    elif m == 'ete3':
+        tree = ete3.Tree(treestr,format=1)
+        t_start = time()
+        leaves = tree.get_leaf_names()
+        tree.get_common_ancestor(leaves)
+        t_end = time()
+
+    elif m == 'compacttree':
+        return NA
+    else:
+        assert False, "Invalid tool: %s"%m
+    return t_end-t_start
+
+# postorder traversal
+def postorder(m):
+    if m == 'dendropy':
+        tree = dendropy.Tree.get(data=treestr, schema='newick')
+        t_start = time()
+        for node in tree.postorder_node_iter():
+            pass
+        t_end = time()
+    elif m == 'biophylo':
+        tree = Phylo.read(treeio, 'newick')
+        t_start = time()
+        for node in tree.find_clades(order='postorder'):
+            pass
+        t_end = time()
+    elif m == 'treeswift':
+        tree = read_tree_newick(treestr)
+        t_start = time()
+        for node in tree.traverse_postorder():
+            pass
+        t_end = time()
+    elif m == 'ete3':
+        tree = ete3.Tree(treestr,format=1)
+        t_start = time()
+        for node in tree.traverse(strategy='postorder'):
+            pass
+        t_end = time()
+
+    elif m == 'compacttree':
+        tree = compact_tree(argv[1][:-3])
+        total = 0.
+        t_start = time()
+        for node in traverse_postorder(tree):
+            total += tree.get_edge_length(node)
+        t_end = time()
+    else:
+        assert False, "Invalid tool: %s"%m
+    return t_end-t_start
+
+# preorder traversal
+def preorder(m):
+    if m == 'dendropy':
+        tree = dendropy.Tree.get(data=treestr, schema='newick')
+        t_start = time()
+        for node in tree.preorder_node_iter():
+            pass
+        t_end = time()
+    elif m == 'biophylo':
+        tree = Phylo.read(treeio, 'newick')
+        t_start = time()
+        for node in tree.find_clades(order='preorder'):
+            pass
+        t_end = time()
+    elif m == 'treeswift':
+        tree = read_tree_newick(treestr)
+        t_start = time()
+        for node in tree.traverse_preorder():
+            pass
+        t_end = time()
+    elif m == 'ete3':
+        tree = ete3.Tree(treestr,format=1)
+        t_start = time()
+        for node in tree.traverse(strategy='preorder'):
+            pass
+        t_end = time()
+    elif m == 'compacttree':
+        tree = compact_tree(argv[1][:-3])
+        total = 0.
+        t_start = time()
+        for node in traverse_preorder(tree):
+            total += tree.get_edge_length(node)
+        t_end = time()
+    else:
+        assert False, "Invalid tool: %s"%m
+    return t_end-t_start
+
+# root distance order traversal
+def rootdistorder(m):
+    if m == 'dendropy':
+        tree = dendropy.Tree.get(data=treestr, schema='newick')
+        t_start = time()
+        tree.calc_node_ages(is_force_max_age=True)
+        for node in tree.ageorder_node_iter(descending=True):
+            pass
+        t_end = time()
+    elif m == 'biophylo':
+        return NA
+    elif m == 'treeswift':
+        tree = read_tree_newick(treestr)
+        t_start = time()
+        for node in tree.traverse_rootdistorder():
+            pass
+        t_end = time()
+    elif m == 'ete3':
+        return NA
+    elif m == 'compacttree':
+        return NA
+    else:
+        assert False, "Invalid tool: %s"%m
+    return t_end-t_start
+
+# total branch length
+def total_branch_length(m):
+    if m == 'dendropy':
+        tree = dendropy.Tree.get(data=treestr, schema='newick')
+        t_start = time()
+        tree.length()
+        t_end = time()
+    elif m == 'biophylo':
+        tree = Phylo.read(treeio, 'newick')
+        t_start = time()
+        tree.total_branch_length()
+        t_end = time()
+    elif m == 'treeswift':
+        tree = read_tree_newick(treestr)
+        t_start = time()
+        tree.edge_length_sum()
+        t_end = time()
+    elif m == 'ete3':
+        tree = ete3.Tree(treestr,format=1)
+        t_start = time()
+        sum(node.dist for node in tree.traverse(strategy='preorder'))
+        t_end = time()
+    if m == 'compacttree':
+        return NA
+    else:
+        assert False, "Invalid tool: %s"%m
+    return t_end-t_start
+
+# load tree
+def load_tree(m):
+    if m == 'dendropy':
+        t_start = time()
+        tree = dendropy.Tree.get(data=treestr, schema='newick')
+        t_end = time()
+    elif m == 'biophylo':
+        t_start = time()
+        tree = Phylo.read(treeio, 'newick')
+        t_end = time()
+    elif m == 'treeswift':
+        t_start = time()
+        tree = read_tree_newick(treestr)
+        t_end = time()
+    elif m == 'ete3':
+        t_start = time()
+        tree = ete3.Tree(treestr,format=1)
+        t_end = time()
+    elif m == 'compacttree':
+        t_start = time()
+        tree = compact_tree(argv[1][:-3])
+        t_end = time()
+    else:
+        assert False, "Invalid tool: %s"%m
+    return t_end-t_start
+
+# memory
+def measure_memory(m):
+    if m == 'dendropy':
+        m_start = memory()
+        t = dendropy.Tree.get(data=treestr, schema='newick')
+        t.encode_bipartitions()
+        m_end = memory()
+    elif m == 'biophylo':
+        m_start = memory()
+        t = Phylo.read(treeio, 'newick')
+        m_end = memory()
+    elif m == 'treeswift':
+        m_start = memory()
+        t = read_tree_newick(treestr)
+        m_end = memory()
+    elif m == 'ete3':
+        m_start = memory()
+        t = ete3.Tree(treestr,format=1)
+        m_end = memory()
+    elif m == 'compacttree':
+        m_start = memory()
+        t = compact_tree(argv[1][:-3])
+        m_end = memory()
+    elseh
+        assert False, "Invalid tool: %s"%m
+    return m_end-m_start
+
+TASKS = {
+    'distance_matrix':distance_matrix,
+    'inorder':inorder,
+    'ladderize':ladderize,
+    'levelorder':levelorder,
+    'load_tree':load_tree,
+    'memory':measure_memory,
+    'mrca':mrca,
+    'postorder':postorder,
+    'preorder':preorder,
+    'rootdistorder':rootdistorder,
+    'total_branch_length':total_branch_length,
+}
+
+# run
+if argv[3] not in TASKS:
+    print("Invalid task. Valid options: %s"%', '.join(sorted(TASKS.keys()))); exit(1)
+print(TASKS[argv[3]](argv[2]))
